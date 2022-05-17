32be58f1e6daf62fe9798082368b71ddaa6f5a94
Sean Magaud <sean.magaud@global-exam.com>
2022-05-17 23:44:43 +0200

    Open Close refactored

--- open-close/main.js
+++ open-close/main.js
@@ -3,41 +3,54 @@
 // This switch case violate the principle, because if we have to add a new quizz type,
 // We have to add a new case, so the function is open and not close as expected
 
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log("1. True");
+    console.log("2. False");
+  }
+}
+class MultipleChoicesQuestion {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
+
+  printQuestionChoices() {
+    this.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option}`);
+    });
+  }
+}
+class TextQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log("___________");
+  }
+}
+
 function printQuiz(questions) {
   questions.forEach((question) => {
     console.log(question.description);
-    switch (question.type) {
-      case "boolean":
-        console.log("1. True");
-        console.log("2. False");
-        break;
-      case "multipleChoice":
-        question.options.forEach((option, index) => {
-          console.log(`${index + 1}. ${option}`);
-        });
-        break;
-      case "text":
-        console.log("Answer: _______");
-        break;
-    }
+    question.printQuestionChoices();
     console.log("");
   });
 }
 
 const questions = [
-  {
-    type: "boolean",
-    description: "Is the sun yellow ?",
-  },
-  {
-    type: "multipleChoice",
-    description: "What is your favorite animal",
-    options: ["Cat", "Dog", "Bird"],
-  },
-  {
-    type: "text",
-    description: "Describe your day",
-  },
+  new BooleanQuestion("Is the sun yellow ?"),
+  new MultipleChoicesQuestion("What is your favorite animal", [
+    "Cat",
+    "Dog",
+    "Bird",
+  ]),
+  new TextQuestion("Describe your day"),
 ];
 
 printQuiz(questions);
