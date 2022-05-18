b89913f2f519d364a989cc9a975bdd631885d936
Sean Magaud <sean.magaud@global-exam.com>
2022-05-18 12:39:07 +0200

    Liskov Substitution refactored

--- liskov-substitution/main.js
+++ liskov-substitution/main.js
@@ -1,34 +1,35 @@
-class Bird {
+class FlyingBird {
   fly() {
     console.log("I can fly");
   }
 }
 
-class Duck extends Bird {
-  quack() {
-    console.log("I can quack");
-  }
-}
-
-class Penguin extends Bird {
-  fly() {
-    console.error("Im a penguin, I cannot fly");
-  }
-
+class SwimmingBird {
   swim() {
     console.log("I can swim");
   }
 }
 
+class Duck extends FlyingBird {}
+// You note that a duck can fly and swim, but we can extend only one classe. We have to use the composition instead of inheritance in this case
+
+class Penguin extends SwimmingBird {}
+
 // The duck can fly but not the penguin.
 // This example violate the liskov substitution principle even if both of these animals are birds
 
-function makeBirdFly(bird) {
+// To understand liskov principle, if you have a function that is accept a class, every single sub class of that class must also be able to enter in that function and workd properly, and should have the same type
+
+function makeFlyingBirdFly(bird) {
   bird.fly();
+}
+
+function makeSwimmingBirdSwim(bird) {
+  bird.swim();
 }
 
 const duck = new Duck();
 const penguin = new Penguin();
 
-makeBirdFly(duck);
-makeBirdFly(penguin);
+makeFlyingBirdFly(duck);
+makeSwimmingBirdSwim(penguin);
